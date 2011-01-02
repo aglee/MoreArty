8e3b5327b925866e9cf47f52c8d4404c3ff09686
Andy Lee <aglee@mac.com>
2011-01-02 15:23:56 -0500

    Code cosmetics.
    Names.  Whitespace.  Grouping and arrangement of methods.  Got rid of ensureLocateDBExists and folded it into awakeFromNib (and I think corrected a bug I introduced).

--- Sources/MoriarityController.h
+++ Sources/MoriarityController.h
@@ -8,15 +8,18 @@
 /*! Controller class that operates as the go-between for the view (the UI) and the model (the locate database).  The point of this code is to show how you can wrap a UNIX task in a nice Cocoa GUI, and get back the output (which you could then operate on or parse further if you choose). */
 @interface MoriarityController : NSObject <TaskWrapperDelegate>
 {
-	IBOutlet id findTextField;
-	IBOutlet id resultsTextField;
-	IBOutlet id window;
-	IBOutlet id relNotesWin;
-	IBOutlet id relNotesTextField;
+	IBOutlet NSWindow *window;
+	IBOutlet NSTextField *searchStringTextField;
 	IBOutlet NSButton *sleuthButton;
-	BOOL findRunning;
+	IBOutlet NSTextView *searchResultsTextView;
+	IBOutlet NSPanel *releaseNotesPanel;
+	IBOutlet NSTextView *releaseNotesTextView;
+	
+	BOOL searchTaskIsRunning;
 	TaskWrapper *searchTask;
 }
+
 - (IBAction)sleuth:(id)sender; // the action that gets executed when the button is pushed
 - (IBAction)displayReleaseNotes:(id)sender; // powers our addition to the help menu
+
 @end
--- Sources/MoriarityController.m
+++ Sources/MoriarityController.m
@@ -6,30 +6,49 @@
 
 @implementation MoriarityController
 
-// here we implement a cheesy check to determine if update.locatedb has been run on the current machine.
-// a fresh Mac OS X install has a very small database file, that contains no useful information.
-- (BOOL)ensureLocateDBExists
+// when first launched, this routine is called when all objects are created
+// and initialized.  It's a chance for us to set things up before the user gets
+// control of the UI.
+-(void)awakeFromNib
 {
-	if ([[NSFileManager defaultManager] fileExistsAtPath:@"/var/db/locate.database"]==YES)
+	searchTaskIsRunning=NO;
+	searchTask=nil;
+	
+	// Lets make sure that there is something valid in the locate database; otherwise,
+	// all searches will come back empty.
+	// here we implement a cheesy check to determine if update.locatedb has been run on the current machine.
+	// a fresh Mac OS X install has a very small database file, that contains no useful information.
+	BOOL locateDBDoesExist = NO;
+	
+	if ([[NSFileManager defaultManager] fileExistsAtPath:@"/var/db/locate.database"])
 	{
-		NSDictionary *attr=[[NSFileManager defaultManager] attributesOfItemAtPath:@"/var/db/locate.database" error:NULL];
-		if (attr==nil || [attr fileSize]>4096)//we pick some size that seems large enough that it couldn't be empty
-			return YES;
-		else
-			return NO;
+		NSDictionary *attr=[[NSFileManager defaultManager] attributesOfItemAtPath:@"/var/db/locate.database"
+																			error:NULL];
+		if ([attr fileSize]>4096)//we pick some size that seems large enough that it couldn't be empty
+			locateDBDoesExist = YES;
 	}
-	else
-		return NO;
+	
+	if (!locateDBDoesExist)
+	{
+		// Explain to the user that they need to go update the database as root.
+		// That is, if they want locate to be able to really find *any* file
+		// on their hard drive (perhaps not great for security, but good for usability).
+		NSRunAlertPanel(@"Error",@"Sorry, Moriarity's 'locate' database is missing or empty.  In a terminal, as root run '/usr/libexec/locate.updatedb' and try Moriarity again.", @"OK",NULL,NULL);
+		[NSApp terminate:nil];
+	}
 }
+
+#pragma mark -
+#pragma mark Action methods
 
 // This action kicks off a locate search task if we aren't already searching for something,
 // or stops the current search if one is already running
 - (IBAction)sleuth:(id)sender
 {
-	if (findRunning)
+	if (searchTaskIsRunning)
 	{
 		// This stops the task and calls our callback (-taskWrapper:didFinishTaskWithStatus:)
-		[searchTask stopProcess];
+		[searchTask stopTask];
 		// Release the memory for this wrapper object
 		[searchTask release];
 		searchTask=nil;
@@ -44,11 +63,23 @@
 		// in ourselves as the delegate for this TaskWrapper object, the path
 		// to the command-line tool, and the contents of the text field that 
 		// displays what the user wants to search on
-		searchTask=[[TaskWrapper alloc] initWithDelegate:self arguments:[NSArray arrayWithObjects:@"/usr/bin/locate",[findTextField stringValue],nil]];
+		searchTask=[[TaskWrapper alloc] initWithDelegate:self arguments:[NSArray arrayWithObjects:@"/usr/bin/locate",[searchStringTextField stringValue],nil]];
 		// kick off the process asynchronously
-		[searchTask startProcess];
+		[searchTask startTask];
 	}
 }
+
+// Display the release notes, as chosen from the menu item in the Help menu.
+- (IBAction)displayReleaseNotes:(id)sender
+{
+	// Grab the release notes from the Resources folder in the app bundle, and stuff 
+	// them into the proper text field
+	[releaseNotesTextView readRTFDFromFile:[[NSBundle mainBundle] pathForResource:@"ReadMe" ofType:@"rtf"]];
+	[releaseNotesPanel makeKeyAndOrderFront:self];
+}
+
+#pragma mark -
+#pragma mark TaskWrapperDelegate methods
 
 // This callback is implemented as part of conforming to the TaskWrapperDelegate protocol.
 // It will be called whenever there is output from the TaskWrapper.
@@ -56,7 +87,7 @@
 {
 	// add the string (a chunk of the results from locate) to the NSTextView's
 	// backing store, in the form of an attributed string
-	[[resultsTextField textStorage] appendAttributedString: [[[NSAttributedString alloc]
+	[[searchResultsTextView textStorage] appendAttributedString: [[[NSAttributedString alloc]
 															  initWithString: output] autorelease]];
 	// setup a selector to be called the next time through the event loop to scroll
 	// the view to the just pasted text.  We don't want to scroll right now,
@@ -69,7 +100,7 @@
 // We now need to scroll the NSScrollView in which the NSTextView sits to the part
 // that we just added at the end
 - (void)scrollToVisible:(id)ignore {
-	[resultsTextField scrollRangeToVisible:NSMakeRange([[resultsTextField string] length], 0)];
+	[searchResultsTextView scrollRangeToVisible:NSMakeRange([[searchResultsTextView string] length], 0)];
 }
 
 // A callback that gets called when a TaskWrapper is launched, allowing us to do any setup
@@ -77,9 +108,9 @@
 // to the TaskWrapperDelegate protocol.
 - (void)taskWrapperDidStartTask:(TaskWrapper *)taskWrapper
 {
-	findRunning=YES;
+	searchTaskIsRunning=YES;
 	// clear the results
-	[resultsTextField setString:@""];
+	[searchResultsTextView setString:@""];
 	// change the "Sleuth" button to say "Stop"
 	[sleuthButton setTitle:@"Stop"];
 }
@@ -89,44 +120,19 @@
 // to the TaskWrapperDelegate protocol.
 - (void)taskWrapper:(TaskWrapper *)taskWrapper didFinishTaskWithStatus:(int)terminationStatus
 {
-	findRunning=NO;
+	searchTaskIsRunning=NO;
 	// change the button's title back for the next search
 	[sleuthButton setTitle:@"Sleuth"];
 }
+
+#pragma mark -
+#pragma mark NSWindow delegate methods
 
 // If the user closes the search window, let's just quit
 -(BOOL)windowShouldClose:(id)sender
 {
 	[NSApp terminate:nil];
 	return YES;
-}
-
-// Display the release notes, as chosen from the menu item in the Help menu.
-- (IBAction)displayReleaseNotes:(id)sender
-{
-	// Grab the release notes from the Resources folder in the app bundle, and stuff 
-	// them into the proper text field
-	[relNotesTextField readRTFDFromFile:[[NSBundle mainBundle] pathForResource:@"ReadMe" ofType:@"rtf"]];
-	[relNotesWin makeKeyAndOrderFront:self];
-}
-
-// when first launched, this routine is called when all objects are created
-// and initialized.  It's a chance for us to set things up before the user gets
-// control of the UI.
--(void)awakeFromNib
-{
-	findRunning=NO;
-	searchTask=nil;
-	// Lets make sure that there is something valid in the locate database; otherwise,
-	// all searches will come back empty.
-	if ([self ensureLocateDBExists]==NO)
-	{
-		// Explain to the user that they need to go update the database as root.
-		// That is, if they want locate to be able to really find *any* file
-		// on their hard drive (perhaps not great for security, but good for usability).
-		NSRunAlertPanel(@"Error",@"Sorry, Moriarity's 'locate' database is missing or empty.  In a terminal, as root run '/usr/libexec/locate.updatedb' and try Moriarity again.", @"OK",NULL,NULL);
-		[NSApp terminate:nil];
-	}
 }
 
 @end
