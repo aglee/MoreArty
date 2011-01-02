933ff2a1b2ddd1fcea1288f8373aff7e892d4a21
Andy Lee <aglee@mac.com>
2011-01-02 14:22:11 -0500

    Break out the boilerplate at the top of each file.
    Moved copyright stuff into LICENSE.txt.  Moved Apple engineers' initials to Credits.html.  Moved class descriptions just before the class declarations and got rid of the redundant copies in the .m files.

--- Sources/MoriarityController.m
+++ Sources/MoriarityController.m
@@ -1,57 +1,10 @@
-/*
- File:		MoriarityController.m
-
- Description: 	This is the implementation for our "controller" class that operates as the go-between for the view (the UI) and the model (the locate database).  The point of this code is to show how you can wrap a UNIX task in a nice Cocoa GUI, and get back the output (which you could then operate on or parse further if you choose).
-
- Author:		MCF
-
- Copyright: 	© Copyright 2001 Apple Computer, Inc. All rights reserved.
-
- Disclaimer:	IMPORTANT:  This Apple software is supplied to you by Apple Computer, Inc.
- ("Apple") in consideration of your agreement to the following terms, and your
- use, installation, modification or redistribution of this Apple software
- constitutes acceptance of these terms.  If you do not agree with these terms,
- please do not use, install, modify or redistribute this Apple software.
-
- In consideration of your agreement to abide by the following terms, and subject
- to these terms, Apple grants you a personal, non-exclusive license, under Apple’s
- copyrights in this original Apple software (the "Apple Software"), to use,
- reproduce, modify and redistribute the Apple Software, with or without
- modifications, in source and/or binary forms; provided that if you redistribute
- the Apple Software in its entirety and without modifications, you must retain
- this notice and the following text and disclaimers in all such redistributions of
- the Apple Software.  Neither the name, trademarks, service marks or logos of
- Apple Computer, Inc. may be used to endorse or promote products derived from the
- Apple Software without specific prior written permission from Apple.  Except as
- expressly stated in this notice, no other rights or licenses, express or implied,
- are granted by Apple herein, including but not limited to any patent rights that
- may be infringed by your derivative works or by other works in which the Apple
- Software may be incorporated.
-
- The Apple Software is provided by Apple on an "AS IS" basis.  APPLE MAKES NO
- WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED
- WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND OPERATION ALONE OR IN
- COMBINATION WITH YOUR PRODUCTS.
-
- IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR
- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-                        GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION
- OF THE APPLE SOFTWARE, HOWEVER CAUSED AND WHETHER UNDER THEORY OF CONTRACT, TORT
- (INCLUDING NEGLIGENCE), STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN
- ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- 
- Version History: 1.1/1.2 released to fix a few bugs (not always removing the notification center,
-                                                  forgetting to release in some cases)
-
- */
-
+// Based on Apple's "Moriarity" sample code at
+// <http://developer.apple.com/library/mac/#samplecode/Moriarity/Introduction/Intro.html>
+// See the accompanying LICENSE.txt for Apple's original terms of use.
 
 #import "MoriarityController.h"
 
 @implementation MoriarityController
-
 
 // here we implement a cheesy check to determine if update.locatedb has been run on the current machine.
 // a fresh Mac OS X install has a very small database file, that contains no useful information.
