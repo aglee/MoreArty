b0cf1116cfdaa3afbbb66cdc9c5d3cd2f1717362
Andy Lee <aglee@mac.com>
2011-01-02 14:01:08 -0500

    Initial commit.  Bring files as-is from Moriarity.

---
+++ Sources/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MorERT
+//
+//  Created by Andy Lee on 1/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
